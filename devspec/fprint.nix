3dea3cce5b613f1b7b82afc7262353cd534c94d3
celet-ff-io <connectingstarts@outlook.com>
2026-01-30 06:28:16 +0800

    fprint on Hyprland

--- devspec/fprint.nix
+++ devspec/fprint.nix
@@ -1,9 +1,13 @@
 # Finger print
-{...}: {
-  services.fprintd.enable = true;
+{ config, lib, ... }: {
+  config = {
+    services.fprintd.enable = true;
 
-  security.pam.services = {
-    login.fprintAuth = true;
-    sudo.fprintAuth = true;
+    security.pam.services = {
+      login.fprintAuth = true;
+      sudo.fprintAuth = true;
+      polkit-1 =
+        lib.mkIf config.programs.hyprland.enable { fprintAuth = true; };
+    };
   };
 }
