29a028a3ed010cfe32086597b25c7ba389c82751
celet-ff-io <connectingstarts@outlook.com>
2026-01-29 09:18:04 +0800

    Git

--- common/users.nix
+++ common/users.nix
@@ -6,14 +6,19 @@ userName: {pkgs, ...}: {
     extraGroups = [
       "wheel"
       "tss"
+      "git"
     ];
     shell = pkgs.zsh;
   };
 
+  users.groups.git = {};
+
   security.sudo.wheelNeedsPassword = false;
 
   systemd.tmpfiles.rules = [
-    "d /etc/nixos 2755 ${userName} wheel - -"
-    "Z /etc/nixos -    ${userName} wheel - -"
+    "d /etc/nixos 2755 ${userName} wheel  - -"
+    "Z /etc/nixos -    ${userName} wheel  - -"
+    "d /srv/git   2775 ${userName} git    - -"
+    "Z /srv/git   -    ${userName} git    - -"
   ];
 }
