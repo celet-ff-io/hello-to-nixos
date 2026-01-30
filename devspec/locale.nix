67e270a777b2a46e545c7d7dbfaf9b89e73cbf95
celet-ff-io <connectingstarts@outlook.com>
2026-01-30 12:08:13 +0800

    Set en_US locale as default

--- devspec/locale.nix
+++ devspec/locale.nix
@@ -1,8 +1,8 @@
 # Locale config
-{...}: {
+{ lib, ... }: {
   # Set your time zone.
   time.timeZone = "Asia/Shanghai";
 
   # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
+  i18n.defaultLocale = lib.mkDefault "en_US.UTF-8";
 }
