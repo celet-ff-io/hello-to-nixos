51a763bbf4edd1a2955262a25db2f304b4455939
celet-ff-io <connectingstarts@outlook.com>
2026-01-31 09:19:43 +0800

    Remove texlive tools

--- common/optional/documents.nix
+++ common/optional/documents.nix
@@ -1,20 +1,6 @@
 # Documents
-{pkgs, ...}: {
+{ pkgs, ... }: {
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [
-    chafa
-    imagemagick
-
-    ghostscript
-    mermaid-cli
-    (texlive.combine {
-      inherit
-        (texlive)
-        scheme-medium
-        latexmk
-        collection-langchinese
-        ;
-    })
-  ];
+  environment.systemPackages = with pkgs; [ chafa imagemagick ];
 }
