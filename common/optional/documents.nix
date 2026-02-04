f6af273877b7d4763c8640c06f6554b4b0934906
celet-ff-io <connectingstarts@outlook.com>
2026-02-04 17:14:17 +0800

    Add GIMP and LibreOffice

--- common/optional/documents.nix
+++ common/optional/documents.nix
@@ -1,6 +1,11 @@
 # Documents
-{ pkgs, ... }: {
+{ config, pkgs, ... }: {
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs; [ chafa imagemagick ];
+  environment.systemPackages = with pkgs;
+    [ chafa imagemagick ] ++ (if config.hasGui then [
+      libreoffice-qt-fresh
+      gimp-with-plugins
+    ] else
+      [ ]);
 }
