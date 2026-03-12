b3e1377e7b9d3439a11a75de30422fdb00213428
celet-ff-io <connectingstarts@outlook.com>
2026-03-12 09:07:16 +0800

    Add programs for notes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -9,6 +9,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 ## [Unreleased]
 
+## 2.2.0 - 2026-03-12
+
+### Added
+
+- Add `nb` and `zk`.
+
 ### Fixed
 
 - Correct outdated descriptions in `./template-configuration.nix`.
--- modules/common/optional/documents.nix
+++ modules/common/optional/documents.nix
@@ -1,11 +1,23 @@
 # Documents
-{ config, pkgs, ... }: {
+{ config, pkgs, ... }:
+{
   # List packages installed in system profile.
   # You can use https://search.nixos.org/ to find more packages (and options).
-  environment.systemPackages = with pkgs;
-    [ chafa imagemagick ] ++ (if config.hasGui then [
-      libreoffice-qt-fresh
-      gimp-with-plugins
-    ] else
-      [ ]);
+  environment.systemPackages =
+    with pkgs;
+    [
+      chafa
+      imagemagick
+      nb
+      zk
+    ]
+    ++ (
+      if config.hasGui then
+        [
+          libreoffice-qt-fresh
+          gimp-with-plugins
+        ]
+      else
+        [ ]
+    );
 }
