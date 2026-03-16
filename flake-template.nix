288c18a35d1f3245be049a724e382afed9a83005
celet-ff-io <connectingstarts@outlook.com>
2026-03-16 19:52:08 +0800

    Correct flake.nix template

--- flake-template.nix
+++ flake-template.nix
@@ -4,7 +4,7 @@
   inputs = {
     nixpkgs.url = "git+https://mirrors.tuna.tsinghua.edu.cn/git/nixpkgs.git?ref=nixos-unstable&shallow=1";
     hello-to-nixos = {
-      url = "git+file:/path/to/hello-to-nixos";
+      url = "git+file:///path/to/hello-to-nixos";
       flake = false;
     };
   };
@@ -18,44 +18,52 @@
     {
       nixosConfigurations."nixoshost" = nixpkgs.lib.nixosSystem {
         modules = [
+          ./common.nix
+
+          # Modules
+
           (import "${hello-to-nixos}/modules/devspec/commonhw.nix") # systemd boot
           (import "${hello-to-nixos}/modules/devspec/hw/wireless-adapter.nix")
-          (import "${hello-to-nixos}/modules/devspec/hw/thunderbolt.nix")
-          (import "${hello-to-nixos}/modules/devspec/hw/i2c.nix")
-          (import "${hello-to-nixos}/modules/devspec/hw/cpu/intel.nix") # Enable kvm-intel
-          (import "${hello-to-nixos}/modules/devspec/hw/gpu/nvidia") # Enable NVIDIA
-          (import "${hello-to-nixos}/modules/devspec/luks.nix" {
-            deviceLuksProvides = "cryptroot";
-            deviceLuksOn = "/dev/disk/by-uuid/926b1085-41a7-48be-821d-4c2eca5ced26";
-          })
+          # (import "${hello-to-nixos}/modules/devspec/hw/thunderbolt.nix")
+          # (import "${hello-to-nixos}/modules/devspec/hw/i2c.nix")
+          # (import "${hello-to-nixos}/modules/devspec/hw/cpu/intel.nix") # Enable kvm-intel
+          # (import "${hello-to-nixos}/modules/devspec/hw/cpu/amd.nix") # Enable kvm-amd
+          # (import "${hello-to-nixos}/modules/devspec/hw/gpu/intel") # Enable i915
+          # (import "${hello-to-nixos}/modules/devspec/hw/gpu/amd") # Enable amdgpu
+          # (import "${hello-to-nixos}/modules/devspec/hw/gpu/nvidia/disable.nix") # Disable NVIDIA
+          # (import "${hello-to-nixos}/modules/devspec/luks.nix" {
+          #   deviceLuksProvides = "luksDevice0";
+          #   deviceLuksOn = "/dev/disk/by-uuid/<uuid>";
+          # })
 
           (import "${hello-to-nixos}/modules/common/os-builder.nix" "x86_64-linux") # Experimental features and mirrors
-          (import "${hello-to-nixos}/modules/devspec/locale.nix") # Timezone and language
+          (import "${hello-to-nixos}/modules/devspec/locale.nix") # Locale
           (import "${hello-to-nixos}/modules/devspec/watchdog.nix") # Watchdog
           (import "${hello-to-nixos}/modules/devspec/printer.nix")
           (import "${hello-to-nixos}/modules/devspec/sound.nix") # pipeware
-          (import "${hello-to-nixos}/modules/devspec/fprint.nix") # fprintd
+          # (import "${hello-to-nixos}/modules/devspec/fprint.nix") # fprintd
+          # (import "${hello-to-nixos}/modules/devspec/battery.nix") # power save
 
           (import "${hello-to-nixos}/modules/devspec/greet.nix") # greetd login
           (import "${hello-to-nixos}/modules/devspec/terminal.nix") # kmscon and more
-          (import "${hello-to-nixos}/modules/devspec/desk/common.nix") # Desktop common
-          (import "${hello-to-nixos}/modules/devspec/desk/hyprland.nix") # Hyprland
-          (import "${hello-to-nixos}/modules/devspec/virtualisation.nix") # Virtualisation (QEMU)
+          # (import "${hello-to-nixos}/modules/devspec/desk/common.nix") # Desktop common
+          # (import "${hello-to-nixos}/modules/devspec/desk/hyprland.nix") # Hyprland
+          # (import "${hello-to-nixos}/modules/devspec/virtualisation.nix") # Virtualisation (QEMU)
+
+          # Enable only if you are using WSL
+          # Please make sure your nixos-wsl in nix-channel is updated to lastest version
+          # or wsl.ssh-agent.enable may not exist and cause error
+          # <nixos-wsl/modules>
+          # (import "${hello-to-nixos}/modules/devspec/wsl.nix" "nixos")
 
           (import "${hello-to-nixos}/modules/common/users.nix" "nixos")
           (import "${hello-to-nixos}/modules/common/basic-software.nix") # nvim, zsh, tmux, git and more
-          (import "${hello-to-nixos}/modules/common/optional")
-
-          ./configuration.nix
-
-          ./userspec/misc.nix
-          ./userspec/cs9711.nix
-          ./userspec/develop.nix
-          ./userspec/ocean.nix
-          ./userspec/steam.nix
-          ./userspec/waydroid.nix
-          ./userspec/netnode.nix
-          ./userspec/neo4j.nix
+          (import "${hello-to-nixos}/modules/common/optional/ssh.nix")
+          # (import "${hello-to-nixos}/modules/common/optional/developer.nix") # cargo and more
+          # (import "${hello-to-nixos}/modules/common/optional/browsers.nix") # browser applications
+          # (import "${hello-to-nixos}/modules/common/optional/documents.nix") # chafa and more
+          # (import "${hello-to-nixos}/modules/common/optional/proxy.nix") # proxy applications
+          # (import "${hello-to-nixos}/modules/common/optional/localsend.nix") # LocalSend
         ];
       };
     };
