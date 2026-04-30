76c33367c3ea0dd12870191505b5e5301117e83f
celet-ff-io <connectingstarts@outlook.com>
2026-04-30 14:20:59 +0800

    Update mirrors

--- modules/common/os-builder.nix
+++ modules/common/os-builder.nix
@@ -7,8 +7,9 @@ hostPlatform:
     "flakes"
   ];
   nix.settings.substituters = lib.mkForce [
+    "https://mirrors.ustc.edu.cn/nix-channels/store"
+    "https://mirrors.sjtug.sjtu.edu.cn/nix-channels/store"
     "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
-    "https://mirror.sjtu.edu.cn/nix-channels/store"
   ];
 
   nixpkgs.config.allowUnfree = true;
